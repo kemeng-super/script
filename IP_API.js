82fee411a7832b8aecd8f2ebc206a0697a1fea25
萌 <57649469+kemeng-super@users.noreply.github.com>
2023-08-23 22:58:05 +0800

    Update IP_API.js

--- IP_API.js
+++ IP_API.js
@@ -45,5 +45,5 @@ var obj = JSON.parse(body);
 var title =flags.get(obj['countryCode']) + ' '+ Area_check(obj['country']) + ' ' + City_ValidCheck(obj['city']) ;
 var subtitle = ISP_ValidCheck(obj['org']);
 var ip = obj['query'];
-var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
+var description = '节点名称:'+title + '\n'+'服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
